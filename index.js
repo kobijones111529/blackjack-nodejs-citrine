b25b6ff50551407bed28c0cef90fe9c222d5b627
Luis Ortiz <l.ortizintec@gmail.com>
2023-04-19 20:15:50 -0400

    added a money feature i still dont know what to do when money runs out

--- index.js
+++ index.js
@@ -1,8 +1,16 @@
-const prompt = require('prompt-sync')()
+const prompt = require('prompt-sync')();
+//this is the starting amount
+let moneyLeft = 100;
+
+let bet;
 
 const deck = [];
 for (let i = 1; i <= 13; i++) {
   deck.push({ number: i, count: 4 });
+}
+
+function random(min, max) {
+  return Math.floor(Math.random() * (max - min + 1) + min);
 }
 
 function valueOfCard(number) {
@@ -18,84 +26,88 @@ function valueOfCard(number) {
 function valueOfHand(cards) {
   let sum = 0;
   for (const card of cards) {
-    sum += card
+    sum += card;
   }
   return sum
 }
 
 function dealCard(deck) {
   const filteredDeck = deck.filter(card => {
-    return card.count > 0
+    return card.count > 0;
   })
   let index = random(1, filteredDeck.length - 1);
-  
-  const card = filteredDeck[index - 1]
-  card.count--
 
-  return valueOfCard(card.number)
+  const card = filteredDeck[index - 1];
+  card.count--;
+
+  return valueOfCard(card.number);
 }
 
-
 function playerTurn(hand) {
-  console.log('Your hand is', hand)
-  let action = prompt('Hit or stay?: ')
+  console.log('Your hand is', hand);
+  let action = prompt('Hit or stay?: ');
 
   while (action === 'hit') {
-    hand.push(dealCard(deck))
-    console.log('Your hand is', hand)
-    action = prompt('Hit or stay?: ')
+    hand.push(dealCard(deck));
+    console.log('Your hand is', hand);
+    action = prompt('Hit or stay?: ');
   }
 
-  return valueOfHand(hand)
+  return valueOfHand(hand);
 }
 
 function dealerTurn(hand) {
   while (valueOfHand(hand) < 17) {
-    hand.push(dealCard(deck))
+    hand.push(dealCard(deck));
   }
-  return valueOfHand(hand)
+  return valueOfHand(hand);
 }
 
 function winner(playerHandValue, dealerHandValue) {
+
   if (playerHandValue > 21) {
-    return 'Player bust'
+    moneyLeft -= Number(bet);
+    return 'Player busts, dealer wins! you now have $' + moneyLeft;
+  } else if (dealerHandValue > 21) {
+    moneyLeft += Number(bet);
+    return 'Dealer busts, player wins! you now have $' + moneyLeft;
   }
 
   if (playerHandValue > dealerHandValue) {
-    return 'Player wins'
+    moneyLeft += Number(bet);
+    return 'Player wins! you now have $' + moneyLeft;
   } else if (dealerHandValue > playerHandValue) {
-    return 'Dealer wins'
+    moneyLeft -= Number(bet);
+    return 'Dealer wins! you now have $' + moneyLeft;
   } else {
-    return 'Draw'
+    return 'Draw! your balance is still $' + moneyLeft;
   }
 }
 
-
 function round() {
-  const playersHand = [dealCard(deck), dealCard(deck)]
-  const dealersHand = [dealCard(deck), dealCard(deck)]
+  const playersHand = [dealCard(deck), dealCard(deck)];
+  const dealersHand = [dealCard(deck), dealCard(deck)];
 
-  const playerHandValue = playerTurn(playersHand)
-  const dealerHandValue = dealerTurn(dealersHand)
+  const playerHandValue = playerTurn(playersHand);
+  const dealerHandValue = dealerTurn(dealersHand);
 
-  console.log('Player hand value:', playerHandValue)
-  console.log('Dealer hand value:', dealerHandValue)
+  console.log('Player hand value:', playerHandValue);
+  console.log('Dealer hand value:', dealerHandValue);
 
-  const roundWinner = winner(playerHandValue, dealerHandValue)
-  console.log(roundWinner)
+  const roundWinner = winner(playerHandValue, dealerHandValue);
+  console.log(roundWinner);
 }
 
 function game() {
-  let playing = true
+  let playing = true;
   while (playing) {
-    round()
-    const playAgain = prompt('Play again? (y/n): ')
-    playing = playAgain === 'y'
+    bet = prompt('Whats your bet? ');
+    round();
+    const playAgain = prompt('Play again? (y/n): ');
+    playing = playAgain === 'y';
   }
 }
 
-game()
+game();
 
-function random(min, max) {
-  return Math.floor(Math.random() * (max - min + 1) + min)
-}
+
